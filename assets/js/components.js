5bd33715b67a897387858a6b6253ce144870399e
pavan <pavankatti81@gmail.com>
2025-11-22 01:37:09 +0530

    Added Testimonials Section

--- assets/js/components.js
+++ assets/js/components.js
@@ -4,7 +4,7 @@
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   })
-  .catch(error => console.error("Error Handling Navbar : ", error))
+  .catch(error => console.error("Error Handling : ", error))
 
 
 // Include Banner in Index
@@ -13,6 +13,7 @@
   .then(data => {
     document.getElementById("banner").innerHTML = data;
   })
+  .catch(error => console.error("Error Handling : ", error))
 
 // Include Controllers to Index.html
   fetch("/assets/components/controllers.html")
@@ -20,6 +21,7 @@
   .then(data => {
     document.getElementById("controllers").innerHTML = data;
   })
+  .catch(error => console.error("Error Handling : ", error))
 
 // Include Footer to Index.html
   fetch("/assets/components/footer.html")
@@ -27,3 +29,22 @@
   .then(data => {
     document.getElementById("footer").innerHTML = data;
   })
+  .catch(error => console.error("Error Handling : ", error))
+
+// Include Reviews
+  fetch("/assets/components/reviews.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("reviews").innerHTML = data;
+
+      const swiper = new Swiper(".reviews-swiper", {
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 20,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      }
+    })
+  })
+  .catch(error => console.error("Error : ", error))
